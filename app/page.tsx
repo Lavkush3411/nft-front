24de2549427066a91dc8b301ddcdface6fb7bc33
sameer2748 <mrao27488@gmail.com>
2024-08-30 15:24:56 +0530

    added api

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+import { transferSol } from "./_apis/AllApis";
 import AddWallet from "./_components/AddWallet";
 import Box from "./_components/Box";
 import Button from "./_components/Buttons";
 import SearchBar from "./_components/SearchBar";
+import SendButton from "./_components/SendButton";
 import TransferToken from "./_components/TransferToken";
 import Wallet from "./_components/Wallet";
 import WalletCarousel from "./_components/WalletCarousel";
 import WalletConnectorPopup from "./_components/WalletConnectorPopup";
 
 export default function Home() {
+  // const receiverPublic = "";
+  // const senderPrivate = "";
+  // const amount=1;
+   
   return (
     <main className="container p-20 gap-20 scrollbar-hide">
+      {/* <SendButton receiverPublic={receiverPublic} senderPrivate={senderPrivate} amount={amount} /> */}
       <WalletConnectorPopup />
       <WalletCarousel />
-      <div className=" flex container gap-20 py-20">
+      <div className="flex container gap-20 py-20">
         <TransferToken />
         <div className="w-full flex flex-col gap-4">
           <SearchBar
